f514ad182d78d2fab0983917f593f008a6e98f35
LucasHaug <lucas.haug@hotmail.com>
2019-09-30 14:31:56 -0300

    Change massage to be sent

--- src/main.c
+++ src/main.c
@@ -24,10 +24,14 @@ int main(void) {
     mcu_init();
 
     for (;;) {
-        sprintf(send_data, "AAAA\r\n");
-        mcu_printf(send_data);
+        for (int i = 0; i <= 13; i++) {
+            sprintf(send_data, "%d", i);
+            mcu_printf(send_data);
+        }
 
-        led_toggle();
-        mcu_sleep(LED_TOGGLE_DELAY_MS);
+        for (int i = 13; i >= 0; i--) {
+            sprintf(send_data, "%d", i);
+            mcu_printf(send_data);
+        }
     }
 }
